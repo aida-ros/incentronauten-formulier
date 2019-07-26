b26937c4b7a8cc26d125f4e0b6c58ce715b69427
AidaRos95 <aidajros@gmail.com>
2019-07-26 15:33:59 +0200

    Set up required fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,82 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 function Form() {
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    dense: {
+      marginTop: theme.spacing(2),
+    },
+    menu: {
+      width: 200,
+    },
+  }));
+  const classes = useStyles();
+
+
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="m">
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-        <h1>Incentronauten Formulier</h1>
+    <main className='form-container'>
+      <h1>Incentronauten Formulier</h1>
+      <TextField
+        required
+        id="outlined-required"
+        label="Voorletters"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        type="text"
+      />
 
+      <TextField
+        required
+        id="outlined-required"
+        label="Achternaam"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        type="text"
+      />
 
+      <TextField
+        required
+        id="outlined-required"
+        label="Postcode"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        type="text"
+      />
 
-      </Container>
-    </React.Fragment>
+      <TextField
+        required
+        id="outlined-required"
+        label="Huisnummer"
+        defaultValue="Huisnummer"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        type="number"
+      />
+
+      <TextField
+        required
+        id="outlined-required"
+        label="E-mail adres"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        type="email"
+      />
+
+    </main>
   );
 }
 
