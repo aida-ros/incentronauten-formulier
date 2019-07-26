d2ea73f40e15e45f97667d91e8209dd0c9a93af3
AidaRos95 <aidajros@gmail.com>
2019-07-26 14:59:46 +0200

    Clean set up

---
+++ src/components/FormContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FormContainer extends Component  {
+  render () {
+    return (
+      <div className="FormContainer">
+        
+      </div>
+    );
+  }  
+}
+
+export default FormContainer;
